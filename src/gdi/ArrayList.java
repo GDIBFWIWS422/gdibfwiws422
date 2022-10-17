76650c22df27991b9333f118f4c21b4c06d69d54
Thomas Ströder <thomas.stroeder@email.de>
2022-10-17 11:13:43 +0100

    lecture 221017

---
+++ src/gdi/ArrayList.java
@@ -0,0 +1,48 @@
+package gdi;
+
+public class ArrayList {
+    int[] list;
+
+    void add(final int value) {
+        final int[] newList = new int[this.list.length + 1];
+        for(int i=0; i < this.list.length; i++) {
+            newList[i] = this.list[i];
+        }
+        newList[newList.length-1] = value;
+        this.list=newList;
+    }
+
+    boolean contains(final int value) {
+        for(int i=0; i < this.list.length; i++) {
+            if(this.list[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean isEmpty() {
+        return this.list.length ==0;
+    }
+
+    void remove(final int value) {
+        if(this.contains(value)) {
+        final int[] minusList = new int[this.list.length - 1];
+        boolean found = false;
+        for(int i=0; i < this.list.length; i++) {
+            if (value == this.list[i]) {
+              found = true;
+            }
+            if (found) {
+                minusList[i-1] = this.list[i];
+            }else {
+                minusList[i] = this.list[i];
+            }
+        }
+
+        this.list=minusList;
+        }
+    }
+}
+
+
---
+++ src/gdi/List.java
@@ -0,0 +1,30 @@
+package gdi;
+
+public class List {
+
+    class ListElement {
+
+        //ListElement previous;
+        ListElement next;
+        int value;
+
+    }
+
+    ListElement root;
+
+    void add(int v) {
+
+    }
+
+    boolean contains(int v) {
+
+    }
+
+    boolean isEmpty() {
+
+    }
+
+    void remove(int v) {
+
+    }
+}
