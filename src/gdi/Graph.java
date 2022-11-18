a14e36cbcc429a055d2691722c6e28523f5d0133
Thomas Ströder <thomas.stroeder@email.de>
2022-11-18 16:13:09 +0000

    dfs and bfs

--- src/gdi/Graph.java
+++ src/gdi/Graph.java
@@ -1,5 +1,7 @@
 package gdi;
 
+import java.util.*;
+
 public class Graph {
 
     String[] vertices;
@@ -23,6 +25,77 @@ public class Graph {
                 edges[i][j] = oldEdges[i][j];
             }
         }
+    }
+    
+    void addEdge(String a, String b, int value) {
+        edges[getVertex(a)][getVertex(b)] = value;
+    }
+    
+    int getVertex(String label) {
+        for(int i=0; i< vertices.length; i++) {
+            if(vertices[i].equals(label)) return i;
+        } 
+        return -1;
+    }
+    
+    void removeEdge(String a, String b) {
+        edges[getVertex(a)][getVertex(b)] = null;
+    }
+    
+    String depthFirstSearch(String start) {
+        return dfs(getVertex(start),new List());
+    }
+    
+    String dfs(int current, List used) {
+        if(used.contains(current)) {
+            return "";
+        }
+        used.add(current);
+        String result = vertices[current];
+        int[] next = getNextVertices(current);
+        for(int i = 0; i<next.length;i++) {
+            result = result + dfs(next[i],used);
+        }
+        return result;
+    }
+    
+    int[] getNextVertices(int vertex) {
+       int size = 0;
+        for(int i = 0; i<edges.length; i++) {
+           if(edges[vertex][i]!= null) {
+               size++;
+           }
+       } 
+       int[] result = new int[size];
+       int index = 0;
+       for(int i = 0; i<edges.length; i++) {
+           if(edges[vertex][i]!= null) {
+               result[index] = i;
+               index++;
+           }
+       } 
+       return result;
+       
+    }
+    
+    String breadthFirstSearch(String start) {
+        List used = new List();
+        Queue<Integer> queue = new LinkedList<>();
+        int current = getVertex(start);
+        queue.offer(current);
+        used.add(current);
+        String result = "";
+        while(!queue.isEmpty()) {
+            current = queue.poll();
+            result = result + vertices[current];
+            int[] next = getNextVertices(current);
+            for(int i = 0; i < next.length; i++) {
+                if(!used.contains(next[i])) {
+                    queue.offer(next[i]);
+                }
+            }
+        }
+        return result;
     }
     
 }
